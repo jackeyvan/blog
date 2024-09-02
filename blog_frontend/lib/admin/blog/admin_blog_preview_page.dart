0259ecead494bb992e566871250032db9f4f77fd
alex <xmpinger@gmail.com>
2024-09-02 08:21:01 +0800

    Update blog preview.

--- blog_frontend/lib/admin/blog/admin_blog_preview_page.dart
+++ blog_frontend/lib/admin/blog/admin_blog_preview_page.dart
@@ -1,6 +1,9 @@
+import 'package:blog_frontend/api/blog_repository.dart';
+import 'package:blog_frontend/model/blog_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_core/get_core.dart';
+import 'package:markdown_widget/markdown_widget.dart';
 
 class AdminBlogPreviewBinding extends Bindings {
   @override
@@ -8,13 +11,41 @@ class AdminBlogPreviewBinding extends Bindings {
 }
 
 class AdminBlogPreviewController extends BaseController {
+  final Rx<BlogModel> blog = BlogModel().obs;
+
+  BlogModel? get blogModel => blog.value;
+
   @override
-  void onReady() {}
+  void onReady() {
+    final id = int.parse(Get.parameters["id"] ?? "0");
+    BlogRepository.fetchBlog(id).then((value) {
+      blog.value = value;
+    });
+  }
 }
 
 class AdminBlogPreviewPage extends BasePage<AdminBlogPreviewController> {
   @override
   Widget buildPage(BuildContext context) {
-    return Text("博客主页");
+    return Scaffold(
+      appBar: AppBar(
+          title: Obx(() => Text("${controller.blogModel?.title}")),
+          centerTitle: true,
+          automaticallyImplyLeading: false),
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Row(children: [
+          Expanded(child: SizedBox(), flex: 1),
+          Expanded(
+              child: SingleChildScrollView(
+                child: Obx(
+                  () =>
+                      MarkdownWidget(data: "${controller.blogModel?.content}"),
+                ),
+              ),
+              flex: 1),
+          Expanded(child: SizedBox(), flex: 1),
+        ]);
+      }),
+    );
   }
 }
